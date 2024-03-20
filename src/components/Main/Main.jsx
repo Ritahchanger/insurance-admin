5f10ee4ccdced26df0fa7d98a1cddfb13fc87790
Dennis Peter <peterdennis573@gmail.com>
2024-03-20 21:14:52 +0300

    Updated navbar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
-    <div className='main'>
-
-        <p className="main-title">MAIN</p>
-
+    <div className="main">
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title font-large" id="exampleModalLabel">
+                MESSAGES
+              </h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <p class="font-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+                rem, dolores omnis labore aliquam mollitia quod fuga tempora
+                obcaecati deleniti eos distinctio, amet quas at! Delectus esse
+                nostrum maxime repudiandae nemo vero ex, nisi quisquam deleniti
+                necessitatibus id recusandae eius!
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary font-medium"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import "./Navbar.scss";
 import Profile from "../../assets/images/profile.webp";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar" id="navbar">
@@ -14,18 +15,25 @@ const Navbar = () => {
           </div>
           <div className="direction-icons flex">
             <p>
-              <i class="fa-solid fa-message"></i>
-            </p>
-            <p>
-              <i class="fa-solid fa-message"></i>
+              <button
+                type="button"
+                id="messages"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                data-bs-whatever="@mdo"
+              >
+                <i class="fa-solid fa-message"></i>
+              </button>
             </p>
             <p>
               <i class="fa-solid fa-bell"></i>
             </p>
           </div>
-           
-          <div className="authentication-btn" >
-            <a href="#" className="btn">LOGOUT</a>
+
+          <div className="authentication-btn">
+            <Link className="btn" to="/signup">
+              LOGOUT
+            </Link>
           </div>
         </div>
       </div>
